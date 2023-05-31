b53dcf77d3f804b7a16849af78512c9e6a94c2c5
ngenoh254 <felixipkoech10@gmail.com>
2023-05-31 15:31:48 +0100

    first commit

---
+++ src/main/java/com/example/demo/appuser/AppUserService.java
@@ -0,0 +1,72 @@
+package com.example.demo.appuser;
+
+import com.example.demo.registration.token.ConfirmationToken;
+import com.example.demo.registration.token.ConfirmationTokenService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.security.Provider;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+@AllArgsConstructor
+@Service
+public class AppUserService implements UserDetailsService {
+
+    private final static String USER_NOT_FOUND_MSG =
+            "user with email %s not found";
+    private final AppUserRepository appUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService ConfirmationTokenService;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                String.format(USER_NOT_FOUND_MSG, email)));
+    }
+public String signUpUser(AppUser appUser){
+    boolean userExists = appUserRepository.findByEmail(appUser.getEmail())
+            .isPresent();
+
+    if (userExists) {
+        throw new IllegalStateException("email already taken");
+    }
+
+    String encodedPassword = bCryptPasswordEncoder
+            .encode(appUser.getPassword());
+
+    appUser.setPassword(encodedPassword);
+
+    appUserRepository.save(appUser);
+
+    String token = UUID.randomUUID().toString();
+
+     //TODO:Send confirmation token
+//    ConfirmationToken confirmationToken =new ConfirmationToken(
+//
+//            token,
+//            LocalDateTime.now(),
+//            LocalDateTime.now().plusMinutes(5),
+//            appUser
+//    );
+
+//  ConfirmationTokenService.saveConfirmationToken(
+//          confirmationToken);
+//
+// //      TODO: SEND EMAIL
+
+    return"token";
+}
+
+    public int enableAppUser(String email) {
+        return appUserRepository.enableAppUser(email);
+}
+}
---
+++ src/main/java/com/example/demo/email/EmailService.java
@@ -0,0 +1,39 @@
+package com.example.demo.email;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@AllArgsConstructor
+public class EmailService implements EmailSender {
+
+    private final static Logger LOGGER = LoggerFactory
+            .getLogger(EmailService.class);
+  private final JavaMailSender mailSender;
+
+    @Override
+    @Async
+    public void send(String to, String email) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+            // Set email properties
+            helper.setTo(to);
+            helper.setText(email, true); // Enable HTML content
+            // Send the email
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            LOGGER.error("Failed to send email", e);
+            throw new IllegalStateException("Failed to send email", e);
+        }
+    }
+}
